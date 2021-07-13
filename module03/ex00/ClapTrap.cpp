e798e36e97c7bb9f1ae852b09b6583262aa58cac
Tony <cardiox1212@hotmail.fr>
2021-07-13 11:00:47 +0200

    Add ClapTrap prefix into messages

--- module03/ex00/ClapTrap.cpp
+++ module03/ex00/ClapTrap.cpp
@@ -5,7 +5,7 @@ ClapTrap::ClapTrap( std::string name ) :
 	m_hit_points( 10 ),
 	m_energy_point( 10 ),
 	m_attack_damage( 0 ) {
-		std::cout << m_name << " is alive!" << std::endl;
+	std::cout << "ClapTrap: " << m_name << " is alive!" << std::endl;
 }
 
 ClapTrap::ClapTrap( const ClapTrap &other ){
@@ -13,31 +13,31 @@ ClapTrap::ClapTrap( const ClapTrap &other ){
 	m_hit_points = other.m_hit_points;
 	m_energy_point = other.m_energy_point;
 	m_attack_damage = other.m_attack_damage;
-	std::cout << m_name << " is alive thanks to other!" << std::endl;
+	std::cout << "ClapTrap: " << m_name << " is alive thanks to other!" << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << m_name << " is dying ... end has never been closer!" << std::endl;
+	std::cout << "ClapTrap: " << m_name << " is dying ... end has never been closer!" << std::endl;
 }
 
 ClapTrap&
 ClapTrap::operator=( const ClapTrap &other ) {
 	if (this != &other)
 		*this = other;
-	std::cout << m_name << " is alive thanks to other!" << std::endl;
+	std::cout << "ClapTrap: " << m_name << " is alive thanks to other!" << std::endl;
 	return *this;
 }
 
 void
 ClapTrap::attack( std::string const &target ) {
-	std::cout << m_name << " attacks " << target << ", causing " << m_hit_points << " points of damage" << std::endl;
+	std::cout << "ClapTrap: " << m_name << " attacks " << target << ", causing " << m_hit_points << " points of damage" << std::endl;
 }
 
 void
 ClapTrap::takeDamage( unsigned int amount ) {
 	unsigned int new_energy_points = m_energy_point - amount;
 
-	std::cout << m_name << " being attacked with " << amount << " damage points." << std::endl;
+	std::cout << "ClapTrap: " << m_name << " being attacked with " << amount << " damage points." << std::endl;
 	if ( new_energy_points < 0 )
 		m_energy_point = 0;
 	m_energy_point = new_energy_points;
@@ -45,6 +45,6 @@ ClapTrap::takeDamage( unsigned int amount ) {
 
 void
 ClapTrap::beRepaired( unsigned int amount ) {
-	std::cout << m_name << " is being repaired with " << amount << " amounts of energy points." << std::endl;
+	std::cout << "ClapTrap: " << m_name << " is being repaired with " << amount << " amounts of energy points." << std::endl;
 	m_energy_point += amount;
 }
