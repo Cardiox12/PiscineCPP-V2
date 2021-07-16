840a8d7cf2d7a5f70074e5f8c6baad9b049edb97
Tony <cardiox1212@hotmail.fr>
2021-07-16 12:08:30 +0200

    Finish ex01 module03

---
+++ module03/ex01/ScavTrap.hpp
@@ -0,0 +1,22 @@
+#ifndef ScavTrap_HPP
+# define ScavTrap_HPP
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	private:
+		
+		ScavTrap();
+	public:
+		ScavTrap( std::string name );
+		virtual ~ScavTrap();
+		ScavTrap(const ScavTrap &src);
+		ScavTrap &operator=(const ScavTrap &src);
+
+		void attack( const std::string &name );
+		void beRepaired( unsigned int amount );
+		void takeDamage( unsigned int amount );
+		void guardGate();
+};
+
+#endif
