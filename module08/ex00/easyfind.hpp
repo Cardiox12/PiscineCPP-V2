ce7a99714404cc4e61cc3d41e8b5cf9f4839f401
Tony <cardiox1212@hotmail.fr>
2021-08-03 21:11:02 +0200

    Add ex00 module08

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -2,17 +2,11 @@
 # define EASYFIND_HPP
 
 #include <iterator>
+#include <algorithm>
 
-template<template<class, class> class Container, class I>
-typename Container<I, std::allocator<int> >::iterator 
-easyfind(Container<I, std::allocator<I> > &container, int n){
-	typename Container<I, std::allocator<int> >::iterator it = container.begin();
-
-	for ( ; it != container.end() ; ++it ){
-		if ( *it == n )
-			break;
-	}
-	return it;
+template<typename T>
+typename T::iterator easyfind(T &container, int n){
+	return ( std::find( container.begin(), container.end(), n ) );
 }
 
 #endif
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -1,11 +1,14 @@
 #include "easyfind.hpp"
 #include <vector>
-#include <array>
+#include <list>
 #include <iostream>
+#include <set>
 
-void test_vector(){
+void test_vector(){	
 	std::vector<int>::iterator it;
 	std::vector<int> vec;
+
+	std::cout << "=================== Vector ===================" << std::endl;
 
 	for ( int index = 0 ; index < 10 ; index++ ){
 		vec.push_back(index);
@@ -26,29 +29,60 @@ void test_vector(){
 	}
 }
 
-void test_array(){
-	std::array<int, 10> arr;
+void test_list() {
+	std::list<int>::iterator it;
+	std::list<int> list;
+
+	std::cout << "=================== List ===================" << std::endl;
 
 	for ( int index = 0 ; index < 10 ; index++ ){
-		arr[index] = index;
+		list.push_back(index);
 	}
 
-	it = easyfind( arr, 2 );
-	if ( it != arr.end() ){
+	it = easyfind(list, 2);
+	if ( it != list.end() ){
 		std::cout << "Found : " << *it << std::endl;
 	} else {
 		std::cout << "Not found" << std::endl;
 	}
 
-	it = easyfind( arr, 100 );
-	if ( it != arr.end() ){
+	it = easyfind( list, 100 );
+	if ( it != list.end() ){
 		std::cout << "Found : " << *it << std::endl;
 	} else {
 		std::cout << "Not found" << std::endl;
 	}
 }
 
+void test_set(){
+	std::set<int>::iterator it;
+	std::set<int> set;
+
+	std::cout << "=================== Set ===================" << std::endl;
+
+	for ( int index = 0 ; index < 10 ; index++ ){
+		set.insert(index);
+	}
+
+	it = easyfind(set, 2);
+	if ( it != set.end() ){
+		std::cout << "Found : " << *it << std::endl;
+	} else {
+		std::cout << "Not found" << std::endl;
+	}
+
+	it = easyfind( set, 100 );
+	if ( it != set.end() ){
+		std::cout << "Found : " << *it << std::endl;
+	} else {
+		std::cout << "Not found" << std::endl;
+	}
+}
+
+
 int		main(void){
 	test_vector();
+	test_list();
+	test_set();
 	return (0);
 }
