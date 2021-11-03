4a46791b31e1e7f497d11787c3eac269bfedc6e5
Tony <cardiox1212@hotmail.fr>
2021-11-03 16:22:45 +0100

    Add ex01 module04

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -3,8 +3,49 @@
 #include "Cat.hpp"
 
 int     main(){
-    const Animal *j = new Dog();
+	{
+		std::cout << "============ Constructors ============" << std::endl;
+		const Animal *dog = new Dog();
+		const Animal *cat = new Cat();
+		
+		std::cout << std::endl << "============ Destructors ============" << std::endl;
+		delete dog;
+		delete cat;
+	}
+	{
+		Animal *animals[4];
 
-    delete j;
-    return (0);
+		std::cout << std::endl << "============ Constructors ============" << std::endl;
+		for ( int index = 0 ; index < 4 ; index++ ){
+			if ( index < 2 ){
+				animals[index] = new Dog();
+			} else {
+				animals[index] = new Cat();
+			}
+		}
+
+		std::cout << std::endl << "============ Types ============" << std::endl;
+		for ( int index = 0 ; index < 4 ; index++ ){
+			std::cout << "Animal type : " << animals[index]->getType() << std::endl;
+		}
+
+		std::cout << std::endl << "============ Sounds ============" << std::endl;
+		for ( int index = 0 ; index < 4 ; index++ ){
+			animals[index]->makeSound();
+		}
+
+		std::cout << std::endl << "============ Destructors ============" << std::endl;
+		for ( int index = 0 ; index < 4 ; index++ ){
+			delete animals[index];
+		}
+	}
+	{
+		std::cout << std::endl << "============ Copies ============" << std::endl;
+		Dog dog;
+		{
+			// If deep copy is bad, it should delete pointer in dog, creating a leak
+			Dog tmp(dog);
+		}
+	}
+	return (0);
 }
--- module04/ex02/src/Animal.cpp
+++
@@ -1,8 +0,0 @@
-#include "Animal.hpp"
-
-Animal::~Animal() {};
-
-const std::string&
-Animal::getType() const {
-	return this->m_type;
-}
