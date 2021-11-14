c98372bc4c431476b3b0b6a37a1db4d331279bd7
Tony BELLAVIA <bbellavi@e1r3p9.42.fr>
2021-11-14 12:33:17 +0100

    AFix bug on function as parameter and add test for complex type on module

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <cstddef>
 
-template<typename T, typename F>
-void iter(T *array, std::size_t size, F f){
+template<typename T>
+void iter(T *array, std::size_t size, void (*f)(const T&)){
 	for ( std::size_t index = 0 ; index < size ; index++ ){
 		f( array[index] );
 	}
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -2,34 +2,54 @@
 #include <string>
 #include "iter.hpp"
 
-void print_str_item(const std::string &item){ std::cout << item << std::endl; }
-void print_cs_item(const char *item) { std::cout << item << std::endl; }
-void print_int_item(int item) { std::cout << item << std::endl; }
+template<typename T>
+void print_item(const T &item){
+	std::cout << item << std::endl;
+}
+
+struct Name {
+	std::string	m_name;
+	Name() : m_name("bbellavi") { }
+};
+
+std::ostream &operator<<(std::ostream &os, const Name &name){
+	os << name.m_name;
+
+	return os;
+}
 
 void test_strings(){
 	std::string items[3] = { "A", "B", "C" };
 	
-	iter(items, 3, print_str_item);
+	iter(items, 3, print_item);
 }
 
 void test_cs(){
 	const char *items[3] = { "A", "B", "C" };
 
-	iter(items, 3, print_cs_item);
+	iter(items, 3, print_item);
 }
 
 void test_int(){
 	int items[3] = { 1, 2, 3 };
 
-	iter(items, 3, print_int_item);
+	iter(items, 3, print_item);
 }
 
+void test_complex_type(){
+	Name names[5];
+	
+	iter(names, 5, print_item);
+}
 
 int	main(void){
+
 	test_strings();
 	std::cout << "============================" << std::endl;
 	test_cs();
 	std::cout << "============================" << std::endl;
 	test_int();
+	std::cout << "============================" << std::endl;
+	test_complex_type();
 	return (0);
 }
